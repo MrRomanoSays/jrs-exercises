8f948c8e6971a8a45c7d0d8542f6a3ef739f4fa4
Lawrence (Joe) Romano <l.j.romano@gmail.com>
2017-05-04 18:00:26 -0400

    Reconfigured code
    Recognized array's needed to check their numbers separately.  Restructured code to include a second for loop to do that.  Also removed earlier thinking about larger array dictating the for loop.  Changed variable types to use const and let.

--- arrays-23.js
+++ arrays-23.js
@@ -1,28 +1,25 @@
 //23. Write a JavaScript function to find the difference of two arrays.
 
-const arrayOne = [1, 2, 3, 4, 5];
-const arrayTwo = [1, 2, 6, 4, 5, 7, 22, 5, 4, 3];
+const arrayOne = [0, 200, 1, 2, 4, 7, 23, 45];
+const arrayTwo = [1, 2, 6, 4, 5, 7, 22, 5, 4, 3, -23];
 
-const findDifference = function (array1, array2) {
-    if (array1.length > array2.length) {
-        var largerArray = array1;
-        var smallerArray = array2;
-    } else if (array1.length < array2.length) {
-        var largerArray = array2;
-        var smallerArray = array1;
-    } else {
-        var largerArray = array1;
-        var smallerArray = array2;
-    }
-    
+const findDifference = function (array1, array2) {    
+    //Placeholder for values that are different 
     let differences = [];
     
-    for (let i = 0 ; i < largerArray.length ; i++) {
-        if (!smallerArray.includes(largerArray[i])) {
-            differences.push(largerArray[i]);
+    //Checking array1 values against array2
+    for (let i = 0 ; i < array1.length ; i++) {
+        if (!array2.includes(array1[i])) {
+            differences.push(array1[i]);
         }
     }
-return "The differences between the given arrays are: " + differences;
+    //Checking array2 values against array1
+    for (let i = 0 ; i < array2.length ; i++) {
+        if (!array1.includes(array2[i])) {
+            differences.push(array2[i]);
+        }
+    }
+    return "The differences between the given arrays are: " + differences;
 };
 
 findDifference(arrayOne, arrayTwo);
