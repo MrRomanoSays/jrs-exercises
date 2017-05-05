e7fae9ad6bf74a3bdeb049f6f2ed77c892fae4bc
Lawrence (Joe) Romano <l.j.romano@gmail.com>
2017-05-04 21:33:51 -0400

    Create arrays-7.js

---
+++ arrays-7.js
@@ -0,0 +1,9 @@
+// 7. Write a JavaScript program to sort the items of an array.
+
+
+var arr1 = [ 3, 8, 7, 6, 5, -4, 3, 2, 1 ]
+arr1.sort();
+
+const sortArray = function (array) {
+    return array.sort();
+}
