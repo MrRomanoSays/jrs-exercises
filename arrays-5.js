f7e1f1764d29d99755cb03a72ce261ccab7cb7d0
Lawrence (Joe) Romano <l.j.romano@gmail.com>
2017-05-04 20:44:41 -0400

    Create arrays-5.js

---
+++ arrays-5.js
@@ -0,0 +1,9 @@
+// 5. Write a simple JavaScript program to join all elements of the following array into a string.
+
+const myColor = ["Red", "Green", "White", "Black"];
+
+const joinArrayIntoString = function (array) {
+    return array.join()
+}
+
+joinArrayIntoString(myColor)
