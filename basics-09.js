6fe2b5b27ac47a86547fec82f7fabf71b01c1df5
Lawrence (Joe) Romano <l.j.romano@gmail.com>
2017-05-06 09:21:42 -0400

    Update basics-09.js

--- basics-09.js
+++ basics-09.js
@@ -1,13 +1,18 @@
 // 9. Write a JavaScript program to calculate multiplication and division of two numbers (input from user).
 
-const multiplyAndDivide = function (number1, number2) {
-let numbersMultiplied = number1*number2;
-let numbersDivided = number1/number2;
-return number1 + " multiplied by " + number2 + " is " + numbersMultiplied + "\n"
-+ number1 + " divided by " + number2 + " is " + numbersDivided
+const multiplyAndDivide = function () {
+
+  let number1 = prompt("Input your first number.")
+  let number2 = prompt("Input your second, and final, number.")
+  
+  let numbersMultiplied = number1*number2
+  let numbersDivided = number1/number2
+
+  return alert(
+    number1 + " multiplied by " + number2 + " is " + numbersMultiplied + 
+    "\n" + number1 + " divided by " + number2 + " is " + numbersDivided
+  )
+
 }
 
-
-//Incorporate alert and/or confirm for directions to user. 
-//Incorporate prompt to set input from user.
-multiplyAndDivide(10, 2)
+multiplyAndDivide()
