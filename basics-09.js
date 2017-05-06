471b46fdf80470519b81609770b9cf1201005b54
Lawrence (Joe) Romano <l.j.romano@gmail.com>
2017-05-06 09:25:42 -0400

    Function mult. and div. numbers input from user

--- basics-09.js
+++ basics-09.js
@@ -1,18 +1,14 @@
 // 9. Write a JavaScript program to calculate multiplication and division of two numbers (input from user).
 
 const multiplyAndDivide = function () {
-
   let number1 = prompt("Input your first number.")
   let number2 = prompt("Input your second, and final, number.")
-  
   let numbersMultiplied = number1*number2
   let numbersDivided = number1/number2
-
   return alert(
     number1 + " multiplied by " + number2 + " is " + numbersMultiplied + 
     "\n" + number1 + " divided by " + number2 + " is " + numbersDivided
   )
-
 }
 
 multiplyAndDivide()
