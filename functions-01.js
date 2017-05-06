aaff99968ea55df21b8636c0cc2f529b024eeddd
Lawrence (Joe) Romano <l.j.romano@gmail.com>
2017-05-05 23:01:11 -0400

    Create functions-01.js

---
+++ functions-01.js
@@ -0,0 +1,14 @@
+//Write a JavaScript function that reverses a number.
+
+const reverseNumber = function (number) {
+
+    let numberAsString = number.toString()
+    let numberAsArray = numberAsString.split("")
+    let reversedNumberAsArray = numberAsArray.reverse()
+    let reversedNumberAsString = reversedNumberAsArray.join("")
+    
+    return reversedNumberAsString
+    
+}
+
+reverseNumber(123)
