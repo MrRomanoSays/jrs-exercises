b69c3fe2d8bba190ba9c8b4c1efa763838c5033e
Lawrence (Joe) Romano <l.j.romano@gmail.com>
2017-05-13 12:33:11 -0400

    Function calculates exp. value from user inputs
    Overly simplistic.  No protection against poor user inputs.

---
+++ functions-13.js
@@ -0,0 +1,7 @@
+// Write a JavaScript function to compute the value of bn where n is the exponent and b is the bases. Accept b and n from the user and display the result.
+
+const calcExpValue = function (b, n) {
+return Math.pow(b, n)
+}
+
+calcExpValue(2, 3) // ==> 8
