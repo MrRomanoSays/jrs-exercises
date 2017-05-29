73ff9f456ffb3929f75cf61a6c1242a495a256ea
Lawrence (Joe) Romano <l.j.romano@gmail.com>
2017-05-29 18:17:06 -0400

    function checks string for palindrome
    Added similar assignment from Free Code Camp. Their version involved using regular expressions.

--- functions-02.js
+++ functions-02.js
@@ -28,3 +28,30 @@ const palindromeChecker = function (palindrome) {
 //palindromeChecker("Animal loots foliated detail of stool lamina") //==> true
 //palindromeChecker(12321) //==> true
 //palindromeChecker("J") //==>error message
+
+
+/*
+Different example from Free Code Camp (FCC) Curriculum
+
+Directions from FCC:
+Return true if the given string is a palindrome. Otherwise, return false.
+A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing.
+
+Note:
+You'll need to remove all non-alphanumeric characters (punctuation, spaces and symbols) and turn everything lower case in order to check for palindromes.
+We'll pass strings with varying formats, such as "racecar", "RaceCar", and "race CAR" among others.
+We'll also pass strings with special symbols, such as "2A3*3a2", "2A3 3a2", and "2_A3*3#A2".
+
+*/
+
+function palindrome(str) {
+  var lowerCaseString = str.toLowerCase();
+  var formattedString = lowerCaseString.replace(/[!@#$%^&*()_+,.:;'"?/|\-\s]/g, "");
+  var reversedString = formattedString.split("").reverse().join("");
+  if (formattedString === reversedString) {
+    return true;
+  }
+  return false;
+}
+
+palindrome("eye");
